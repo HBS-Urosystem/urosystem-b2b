929c7dc3a36cb36a3a0bfd4241988b9119f6271d
vhollo <vhollo@protonmail.com>
2022-04-25 18:20:49 +0200

    adapter netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte'
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
-export default /** @type {import('astro').AstroUserConfig} */ ({
+export default /** @type {import('astro').AstroUserConfig} */ defineConfig({
 	integrations: [
     tailwind(),
     svelte(),
   ],
+  adapter: netlify(),
 });
+
