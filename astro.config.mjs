d3c54175a2264b1e68cc715f9f3fc59f8d35de8b
vhollo <vhollo@protonmail.com>
2022-05-11 11:56:00 +0200

    edge-functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import svelte from '@astrojs/svelte'
-import netlify from '@astrojs/netlify/functions'
+import netlify from '@astrojs/netlify/edge-functions'
 
 // https://astro.build/config
 export default /** @type {import('astro').AstroUserConfig} */ defineConfig({
