1bfbe58043ded66897fd261084b167e78b9da3fb
vhollo <vhollo@protonmail.com>
2022-05-12 23:32:59 +0200

    base: '/b2b'

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,9 +12,9 @@ export default /** @type {import('astro').AstroUserConfig} */ defineConfig({
   /*adapter: netlify({
     dist: new URL('./dist/', import.meta.url)
   }),*/
-  //outDir: './dist/b2b',
+  outDir: './dist/b2b',
   site: 'https://www.urosystem.com',
-  base: '/b2b/',
+  base: '/b2b',
   trailingSlash: 'ignore'
 });
 
