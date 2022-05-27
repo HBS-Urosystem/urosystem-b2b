b2b73fe0d6102c20072c284ae1a2273e71bc0cd3
vhollo <vhollo@protonmail.com>
2022-05-27 17:28:03 +0200

    no astro adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import svelte from '@astrojs/svelte'
-import netlify from '@astrojs/netlify/functions'
+//import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default /** @type {import('astro').AstroUserConfig} */ defineConfig({
@@ -9,12 +9,12 @@ export default /** @type {import('astro').AstroUserConfig} */ defineConfig({
     tailwind(),
     svelte(),
   ],
-  adapter: netlify({
+  /*adapter: netlify({
     dist: new URL('./dist/', import.meta.url)
-  }),
+  }),*/
   site: 'https://www.urosystem.com',
   //base: '/',
-  outDir: './dist',
+  //outDir: './dist',
   trailingSlash: 'ignore'
 });
 
